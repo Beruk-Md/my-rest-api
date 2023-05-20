47d44c69516efbe8156586aae4189fd588f16114
Beruk-Md <133238768+Beruk-Md@users.noreply.github.com>
2023-05-20 07:38:23 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "api.berukofc.xyz@gmail.com" //email
 global.email_password = "igrtigexiqznbmog" //application password email
 global.limitCount = 10000
-global.YUOR_PORT = 8000
+global.YUOR_PORT = 8080
 global.loghandler = {
 	noapikey:{
 		status: 403,
